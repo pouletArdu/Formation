346594f109c09a0cfc999e460397330303bc70b3
Teddy <teddy.patard@vivaticket.com>
2022-07-18 14:24:07 +0200

    Initialize Domain

--- src/Formation.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Formation.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Formation.Domain/Entities/AuthorDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Formation.Domain.Entities
+{
+    public class AuthorDTO:BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; } 
+        public DateOnly Birthday { get; set; }
+        public Gender Gender { get; set; }
+        public BookDTO[] Books { get; set; }
+
+    }
+}
